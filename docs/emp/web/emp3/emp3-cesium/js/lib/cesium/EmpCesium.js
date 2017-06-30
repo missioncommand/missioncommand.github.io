67ec327d8ba2880de923a1e55e2bef7b7d67d239
missioncommandbot <missioncommandbot@missioncommandbot.org>
2017-06-30 14:32:40 +0000

    Deploying updated GitHub Pages

--- docs/emp/web/emp3/emp3-cesium/js/lib/cesium/EmpCesium.js
+++ docs/emp/web/emp3/emp3-cesium/js/lib/cesium/EmpCesium.js
@@ -6420,33 +6420,28 @@ function EmpCesium() {
   this.addWmtsToMap = function(item) {
     try {
       var layer = this.getLayer(item.coreId),
-        useProxy = false;
+      sLayers = "", options = {},
+      webMapTileServiceImageryProvider,
+      tileMatrixSet = 'default028mm',
+      useProxy = false;
+
       if (layer) {
         this.removeLayer(layer);
       }
-      var options = {};
+
+      tileMatrixSet = (this.defined(item.tileMatrixSet) )?item.tileMatrixSet:tileMatrixSet;
       layer = new EmpLayer(item.name, item.coreId, EmpCesiumConstants.layerType.WMTS_LAYER, this); //empCesium);
       layer.providers = [];
       layer.url = item.url;
       layer.globalType = EmpCesiumConstants.layerType.WMTS_LAYER;
-      var sLayers = "";
-      if (this.isV2Core && item.activeLayers) {
-        sLayers = item.activeLayers.join();
-      } else if (!this.isV2Core && item.layers) {
-        sLayers = item.layers.join();
-      }
+      sLayers = item.layer;
+      useProxy = (this.defined(item.useProxy) )?item.useProxy:useProxy;
 
-      if (this.isV2Core) {
-        useProxy = emp.util.config.getUseProxySetting();
-      } else if (item && item.useProxy) {
-        useProxy = item.useProxy;
-      }
-
-      var webMapTileServiceImageryProvider = new this.WebMapTileServiceImageryProvider({
+        webMapTileServiceImageryProvider = new this.WebMapTileServiceImageryProvider({
         url: layer.url,
         style: 'default',
         format: 'image/jpeg',
-        tileMatrixSetID: 'default028mm',
+        tileMatrixSetID: tileMatrixSet,
         // tileMatrixLabels : ['default028mm:0', 'default028mm:1', 'default028mm:2' ...],
         maximumLevel: 19,
         //credit : new Cesium.Credit('U. S. Geological Survey')
@@ -6459,15 +6454,9 @@ function EmpCesium() {
         imageryLayer: undefined,
         enable: false
       });
-      //}
-      //imageryEmpLayers[imageryEmpLayersIndex] = layer;
-      //imageryEmpLayersOptions[imageryEmpLayersIndex] = {isCors: ("true" === imageryJsonObject.disableProxy.toLowerCase()), isBase: true};
-      // layer.options = imageryEmpLayersOptions[imageryEmpLayersIndex];
-      //imageryEmpLayersOptions[imageryEmpLayersIndex].singleTile = false;
-      //dropDown.options.add(new Option(imageryJsonObject.name));
+
       this.addLayer(layer);
       this.imageryEmpLayers.push(layer);
-      //this.imageryEmpLayersOptions.push(options);
       this.enableLayer(layer, true);
     } catch (err) {
       console.error("Adding WMTS to Cesium failed! ", err);
